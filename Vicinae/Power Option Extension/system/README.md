f1aba11dec0081ebed1dd446b3f05e7da3270279
Tejas <tejasahuja4038@gmail.com>
2025-09-11 01:12:46 +0530

    Added Vicinae Launcher

---
+++ Vicinae/Power Option Extension/system/README.md
@@ -0,0 +1,10 @@
+#Power Options Extension for Vicinae
+
+```bash
+npm run dev         # Run Extension in dev mode
+```
+
+
+```bash
+npm run build       # Install extension in ~/.local/share/vicinae/extensions
+```
---
+++ Vicinae/Power Option Extension/system/src/power.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { List, ActionPanel, Action, showToast, Icon, Toast } from '@vicinae/api';
+
+import { exec } from "child_process";
+import path from "path";
+
+function runCommand(command: string, actionName: string) {
+  exec(command, (error, stdout, stderr) => {
+    if (error || stderr) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: `${actionName} cancelled`,
+        message: error?.message || stderr,
+      });
+    }
+  });
+}
+
+export default function Command() {
+  return (
+    <List>
+      <List.Item
+        title="Shut Down"
+        icon={path.resolve(__dirname, "assets/shutdown.svg")}
+        actions={
+          <ActionPanel>
+            <Action
+              title="Shut Down"
+              onAction={() => runCommand("gnome-session-quit --power-off", "shut down")}
+            />
+          </ActionPanel>
+        }
+      />
+      <List.Item
+        title="Restart"
+        icon={path.resolve(__dirname, "assets/restart.svg")}
+        actions={
+          <ActionPanel>
+            <Action
+              title="Restart"
+              onAction={() => runCommand("gnome-session-quit --reboot", "restart")}
+            />
+          </ActionPanel>
+        }
+      />
+      <List.Item
+        title="Log Out"
+        icon={path.resolve(__dirname, "assets/logout.svg")}
+        actions={
+          <ActionPanel>
+            <Action
+              title="Log Out"
+              onAction={() => runCommand("gnome-session-quit --logout", "log out")}
+            />
+          </ActionPanel>
+        }
+      />
+    </List>
+  );
+}
